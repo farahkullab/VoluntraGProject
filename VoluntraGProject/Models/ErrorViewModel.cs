bdf23a709d3712510e05018aa17c84d2454b79d8
farahkullab <farah21kullab@gmail.com>
2025-01-06 22:19:12 +0300

    Add project files.

---
+++ VoluntraGProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VoluntraGProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
